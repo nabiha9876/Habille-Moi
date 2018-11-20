13e1355313fa47fa1d70002cf8dd325c924a7686
Fin Sampson <finleyjsampson@gmail.com>
2018-11-20 16:40:04 +0000

    Set up
    Imported Firebase and tidied up

--- Habille Moi/ViewController.swift
+++ Habille Moi/ViewController.swift
@@ -1,18 +1,11 @@
-//
-//  ViewController.swift
-//  Habille Moi
-//
-//  Created by Nabiha Rahman on 20/11/2018.
-//  Copyright © 2018 Nabiha Rahman. All rights reserved.
-//
-
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
 
