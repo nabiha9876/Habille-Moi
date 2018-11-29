e2ee16697178fcc2d93f0a71cba8067a500443ea
Fin Sampson <finleyjsampson@gmail.com>
2018-11-29 15:43:56 +0000

    Storyboard needs creating

---
+++ Habille Moi/Assets.xcassets/Podfile
@@ -0,0 +1,8 @@
+project 'Habille Moi.xcodeproj'
+
+target 'Habille Moi' do
+  use_frameworks!
+
+pod 'Firebase/Core'
+
+end
--- Habille Moi/ViewController.swift
+++ Habille Moi/ViewController.swift
@@ -5,9 +5,41 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        weak var topImageView: UIImageView!
+        weak var bottomImageView: UIImageView!
+        
+        var tops = [String]()
+        var bottoms = [String]()
+        
+        //SUMMERCLOTHES
+        func viewDidLoad() {
+            super.viewDidLoad()
+            
+            tops = ["SummerTop-1", "SummerTop-2" , "SummerTop-3", "SummerTop-4",]
+            bottoms = ["SummerBottoms-1", "SummerBottoms-2", "SummerBottoms-3", "SummerBottoms-4"]
+        }
+        
+        
+        
+        func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            
+            guard let top = tops.randomElement(), let bottom = bottoms.randomElement() else { return }
+            topImageView.image = UIImage(named: top)
+            bottomImageView.image = UIImage(named: bottom)
+            
+            
+            
+        }
+        
+        
+        
+    }
+
 
     }
 
 
-}
+
 
