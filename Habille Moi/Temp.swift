efea117cec58f1640d2d881f27521aa8a7f38c4d
Fin Sampson <finleyjsampson@gmail.com>
2018-12-03 20:19:47 +0000

    APIKEY

--- Habille Moi/Temp.swift
+++ Habille Moi/Temp.swift
@@ -10,8 +10,8 @@ import Foundation
 
 struct API {
     
-    static let APIKey = ""
-    static var BaseURL = URL(string: "")!
+    static let APIKey = "8e9ecf16972151233982b4dc8f49f77a"
+    static var BaseURL = URL(string: "https://api.darksky.net/forecast/8e9ecf16972151233982b4dc8f49f77a/37.8267,-122.4233")!
     
     static var AuthenticatedBaseURL: URL {
         return BaseURL.appendPathComponent(APIKey)
@@ -20,8 +20,8 @@ struct API {
     
     struct Defaults {
         
-        static let Latitude: Double =//
-        static let Longtitude: Double =//
+        static let Latitude: Double = 50.742844
+        static let Longtitude: Double = -1.894659
         
     }
 
